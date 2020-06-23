163cf9830bd6837f3494e7904f330481f433c409
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-06-23 14:51:48 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
+// const express = require('express');
+// const server = express();
+
+// const serverPort = process.env.PORT || 1234;
+
+// console.log('dirname', __dirname + '/../build');
+// server.use(express.static(__dirname + '/../build'));
+
+// server.get('/api/hamsters', (req, res) => {
+//     console.log('GET /api', req.url);
+//     let array = getHamstersFromDatabase();
+//     res.send(JSON.stringify(array));
+// })
+
+// server.listen(serverPort, () => console.log('Server is listening on port '  + serverPort));
+
+// function getHamstersFromDatabase() {
+//     return [
+//         { id: 1, name: 'Nils', favFood: 'morot',
+//     loves: 'backendprogrammering' },
+//     { id: 2, name: 'Greta', favFood: 'kål',
+//     loves: 'frontendprogrammering' }
+//     ]
+// }
 const express = require('express');
 const server = express();
 
+// Heroku assigns the server a port -> process.env.port
 const serverPort = process.env.PORT || 1234;
 
 console.log('dirname', __dirname + '/../build');
@@ -12,13 +37,14 @@ server.get('/api/hamsters', (req, res) => {
     res.send(JSON.stringify(array));
 })
 
-server.listen(serverPort, () => console.log('Server is listening on port '  + serverPort));
+// Glöm inte att starta servern med rätt portnummer
+// Man kan se allt som skrivs ut med console.log från Heroku webbsidan
+server.listen(serverPort, () => console.log('Server is listening on port ' + serverPort));
+
 
 function getHamstersFromDatabase() {
     return [
-        { id: 1, name: 'Nils', favFood: 'morot',
-    loves: 'backendprogrammering' },
-    { id: 2, name: 'Greta', favFood: 'kål',
-    loves: 'frontendprogrammering' }
+        { id: 1, name: 'Nils', favFood: 'morot', loves: 'backendprogrammering' },
+        { id: 2, name: 'Greta', favFood: 'kål', loves: 'frontendprogrammering' }
     ]
 }
