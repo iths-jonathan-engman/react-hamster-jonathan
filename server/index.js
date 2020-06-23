a78f1ce0b7da522e1539d10dd8fc5f04cf1c41e9
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-06-23 14:14:37 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,3 @@
-// const express = require('express');
-// const app = express();
-
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.json());
-
-// let path = require('path');
-// app.get('/*', (req, res) => {
-//     let filePath = path.resolve('./build/index.html')
-//     res.sendFile(filePath)
-// })
-// app.use(express.static(__dirname + '/../build'));
-
-// // app.use('/', express.static('public'));
-// app.use('/assets', express.static('assets'));
-
-// const hamstersRoute = require('./routes/hamsters');
-// app.use('/api/hamsters', hamstersRoute);
-
-// const chartsRoute = require('./routes/charts');
-// app.use('/api/charts', chartsRoute);
-
-// const gamesRoute = require('./routes/games');
-// app.use('/api/games', gamesRoute);
-
-// const statsRoute = require('./routes/stats');
-// app.use('/api/stats', statsRoute);
-
-
-// app.listen(PORT, () => {
-//     console.info('Hamsterwars armed and ready on port 3000.')
-// })
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -41,7 +7,6 @@ app.use('/assets', express.static(__dirname + '/pictures'))
 app.use(express.json());
 app.use(express.static(__dirname + '/../build'));
 
-//Require routes
 const chartsRoute = require('./routes/charts');
 app.use('/api/charts', chartsRoute);
 
@@ -61,7 +26,7 @@ app.get('/*', function(req, res) {
       }
     })
   })
-//App listening to port 
+
 app.listen(serverPort, () => {
     console.log(`Get ready for hamster wars on port ${serverPort}!`)
 })
