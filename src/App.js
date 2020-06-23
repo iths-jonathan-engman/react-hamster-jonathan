9a8708de38e8a381b13b32269a4a441145431b02
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-06-23 14:41:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,93 @@
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//     const [hamsters, setHamsters] = useState(null);
+//     const handleClick = async () => {
+//       let array = await getHamsters();
+//       console.log('Get hamsters from api:', array)
+//       setHamsters(array);
+//     }
+//   return (
+//       <div className="App">
+//         <header className="App-header">
+//             <h1> Fullstack demo </h1>
+//         </header>
+//         <main>
+//           <h2> Demonstera API </h2>
+//           <p> 
+//               <button onClick={handleClick}> Hämta hamsterdata </button>
+//           </p>
+//           <div>
+//             { hamsters 
+//               ? hamsters.map(hamster => (
+//                 <div key={hamster.id}>
+//                     {hamster.name} gillar {hamster.loves} och äter 
+//                     helst {hamster.favFood}!
+//                   </div>
+//                 )) 
+//                 : null }
+//           </div>
+//         </main>
+//       </div>
+//   );
+// }
+
+// async function getHamsters() {
+//     let baseUrl = '/api';
+//     try {
+//         const response = await fetch(baseUrl + '/hamsters');
+//         console.log('response ok')
+//         const hamsterArray = await response.json();
+//         console.log('JSON parse ok')
+//         return hamsterArray;
+//     } catch (e) {
+//         console.log('Fetch failed because', e);
+//         return null;
+//     }
+// }
+
+// export default App;
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
     const [hamsters, setHamsters] = useState(null);
     const handleClick = async () => {
-      let array = await getHamsters();
-      console.log('Get hamsters from api:', array)
-      setHamsters(array);
+        let array = await getHamsters();
+        console.log('Got hamsters from api:', array);
+        setHamsters(array);
     }
-  return (
-      <div className="App">
-        <header className="App-header">
-            <h1> Fullstack demo </h1>
-        </header>
-        <main>
-          <h2> Demonstera API </h2>
-          <p> 
-              <button onClick={handleClick}> Hämta hamsterdata </button>
-          </p>
-          <div>
-            { hamsters 
-              ? hamsters.map(hamster => (
-                <div key={hamster.id}>
-                  {hamster.name} gillar {hamster.loves} och äter 
-                  helst {hamster.favFood}!
-                  </div>
-                )) 
-                : null }
-          </div>
-        </main>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> Fullstack demo </h1>
+            </header>
+            <main>
+                <h2> Demonstrera API </h2>
+                <p>
+                    <button onClick={handleClick}> Hämta hamsterdata </button>
+                </p>
+                <div>
+                    { hamsters
+                        ? hamsters.map(hamster => (
+                            <div key={hamster.id}>
+                                {hamster.name} gillar {hamster.loves} och äter helst {hamster.favFood}!
+                            </div>
+                        ))
+                        : null }
+                </div>
+            </main>
+        </div>
+    );
 }
+
 
 async function getHamsters() {
     let baseUrl = '/api';
     try {
         const response = await fetch(baseUrl + '/hamsters');
-        console.log('response ok')
         const hamsterArray = await response.json();
-        console.log('JSON parse ok')
         return hamsterArray;
     } catch (e) {
         console.log('Fetch failed because', e);
