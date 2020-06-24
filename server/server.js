39fc23bdb94d6d09669a797f13ea1f4f30f96531
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-06-24 13:56:27 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.json())
+// app.use(express.json())
 
 const serverPort = process.env.PORT || 1234;
 
