4c180f01a81892c1218f9b9cc8a3ececddd6146f
iths-jonathan-engman <54362354+iths-jonathan-engman@users.noreply.github.com>
2020-06-24 13:07:42 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,46 @@
 const express = require('express');
-const server = express();
+const app = express();
+const path = require('path');
+app.use(express.json())
 
 const serverPort = process.env.PORT || 1234;
 
-console.log('dirname', __dirname + '/../build');
-server.use(express.static(__dirname + '/../build'));
 
-server.get('/api/hamsters', (req, res) => {
+console.log('dirname', __dirname + '/../build');
+app.use(express.static(__dirname + '/../build'));
+
+//Routes
+app.use('/api/', express.static('public'));
+app.use('/api/assets', express.static('assets'));
+
+const hamstersRoute = require('./routes/hamsters');
+app.use('/api/hamsters', hamstersRoute);
+
+const chartsRoute = require('./routes/charts');
+app.use('/api/charts', chartsRoute);
+
+const gamesRoute = require('./routes/games');
+app.use('/api/games', gamesRoute);
+
+const statsRoute = require('./routes/stats');
+app.use('/api/stats', statsRoute);
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../build/index.html'),
+    function(err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
+
+app.get('/api/hamsters', (req, res) => {
     console.log('GET /api', req.url);
     let array = getHamstersFromDatabase();
     res.send(JSON.stringify(array));
 })
 
-server.listen(serverPort, () => console.log('Server is listening on port ' + serverPort));
+app.listen(serverPort, () => console.log('Server is listening on port: ' + serverPort));
 
 
 function getHamstersFromDatabase() {
